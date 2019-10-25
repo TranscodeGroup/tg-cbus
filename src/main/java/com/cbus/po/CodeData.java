da2946475cbca263c41f96691334af0503e1ea38
xiaorongnie <xiaorongnie@transcodegroup.com>
2019-10-25 15:56:54 +0800

    *增加code的set方法

--- src/main/java/com/cbus/po/CodeData.java
+++ src/main/java/com/cbus/po/CodeData.java
@@ -1,30 +1,25 @@
 package com.cbus.po;
 
+import com.cbus.utils.TgCode;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 /**
  * 带错误码的数据类 用于返回给客户端
+ * 
  * @author zhen.lin
- * @data   2017年6月22日 下午1:43:29
+ * @data 2017年6月22日 下午1:43:29
  */
 public class CodeData {
 
     /**
-     * 返回码  
-     * 0    正确  
-     * 1000 未知错误 一般服务器报错时的值 
-     * 1000以上, 具体的错误, 每个code对应一种错误, 不重复; 具体表示什么服务器定
+     * 返回码 0 正确 1000 未知错误 一般服务器报错时的值 1000以上, 具体的错误, 每个code对应一种错误, 不重复;
+     * 具体表示什么服务器定
      *
-     * 以下是通用错误
-     * 1001 KEY错误
-     * 1002 时间戳错误
-     * 1003 签名错误
-     * 1004 参数错误
+     * 以下是通用错误 1001 KEY错误 1002 时间戳错误 1003 签名错误 1004 参数错误
      * 
-     * 1011 新增失败
-     * 1012 更新失败
-     * 1013 删除失败
+     * 1011 新增失败 1012 更新失败 1013 删除失败
      *
-     * 以下是业务相关错误
-     * 1100 用户名或密码错误
+     * 以下是业务相关错误 1100 用户名或密码错误
      */
     private int code;
     /**
@@ -59,6 +54,10 @@ public class CodeData {
 
     public void setResult(Object result) {
         this.result = result;
+    }
+
+    public void setCode(TgCode tgCode) {
+        this.setCode(tgCode.getId());
     }
 
 }
